316f616556c45a3bc7536335678483def6030619
qishichang <qishichang@live.com>
2018-08-10 13:28:21 +1200

    Create a complex data model - Tutorial 5 of 10

--- ContosoUniversity/Migrations/20180810004010_ComplexDataModel.Designer.cs
+++ ContosoUniversity/Migrations/20180810004010_ComplexDataModel.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180810004010_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
